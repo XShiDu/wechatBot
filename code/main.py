8d9eef86cc9bc020c29822d6ab7cce89e766710e
谢诗杜 <2211045@mail.dhu.edu.cn>
2024-03-02 20:46:33 +0800

    多线程解决请求的问题不响应

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import Flask
 from chat_bot import Bot
 import yaml
+import threading
 
 with open('../api_config.yml', 'r', encoding='utf-8') as f:
     apis = yaml.load(f.read(), Loader=yaml.FullLoader)
@@ -18,7 +19,16 @@ def index():
         return bot.get_apply(request)
 
     elif request.method == "POST":
-        return bot.post_apply(request)
+        # 开启一个新线程解决wx五秒不响应请求，失效的问题
+        sub_thread = threading.Thread(target=bot.post_apply(request))
+        sub_thread.start()
+        # 主线程等待子线程4秒
+        sub_thread.join(2)
+
+        if sub_thread.is_alive():
+            return bot.post_apply(request)
+        else:
+            return 'sucess'
 
 
 if __name__ == "__main__":
